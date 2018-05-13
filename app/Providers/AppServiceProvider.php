68ba81b5e973b80405cfe44a3ff1a1ec4461884f
Madinabonu <123_madinka@mail.ru>
2018-05-13 13:14:56 +0500

    creating db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,9 @@
- <?php
+<?php
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
